5fb683ad467bfdbb2cce7c3d48d95da1f529626a
Steve <steven.p.gantz@gmail.com>
2015-02-20 10:19:45 -0500

    Created Main menu & Help page
    Main menu and help page are somewhat empty and have no permanent
    objects. Developer and IDE defined functions are permanent.

--- app/src/main/java/com/csc354cde335/kuselftour/HelpPage.java
+++ app/src/main/java/com/csc354cde335/kuselftour/HelpPage.java
@@ -1,41 +1,29 @@
 package com.csc354cde335.kuselftour;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 /**
- * Steven Gantz
- * Main menu class, handles activity transfer to one of three main activities.
- * Camera based AR, 2D top down map view, and a help screen.
+ * The help page will be non-interactive. This activity
+ * exists only to assist the user in understanding the
+ * usage of the application. All methods below were
+ * IDE generated.
  */
+public class HelpPage extends ActionBarActivity {
 
-public class MainMenu extends ActionBarActivity {
-
-    /**
-     * Override oncreate of activity
-     * (Runs when the MainMenu class is initially created)
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_help_page);
     }
 
-    /**
-     * Runs when the options menu is created
-     * (Top right corner)
-     * @param menu
-     * @return
-     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_help_page, menu);
         return true;
     }
 
@@ -52,17 +40,5 @@ public class MainMenu extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    // Completely developer created methods below.
-    // Automated methods created above
-
-    public void openHelp(View view) {
-        Intent intent = new Intent(this, HelpPage.class);
-        startActivity(intent);
-    }
-
-    public void openAR(View view) {
-
     }
 }
